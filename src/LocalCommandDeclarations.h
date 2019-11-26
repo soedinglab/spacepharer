c1ad3fc21fb978129f0fbe0979761db9d451f528
Ruoshi Zhang <rosyzhang9701@gmail.com>
2019-11-26 17:26:55 +0100

    Update command declarations

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -7,7 +7,9 @@ extern int predictmatch(int argc, const char **argv, const Command& command);
 extern int createsetdb(int argc, const char **argv, const Command& command);
 extern int parsespacer(int argc, const char **argv, const Command& command);
 extern int filtermatchbyfdr(int argc, const char **argv, const Command& command);
+extern int findpam(int argc, const char **argv, const Command& command);
 extern int truncatebesthits(int argc, const char **argv, const Command& command);
-extern int downloadgbphage(int argc, const char **argv, const Command& command);
+extern int summarizeresults(int argc, const char **argv, const Command& command);
+extern int downloadgenome(int argc, const char **argv, const Command& command);
 
 #endif
