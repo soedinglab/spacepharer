3b1b1478e5d995a8f7ef46ed94008e9983a692a0
Milot Mirdita <milot@mirdita.de>
2020-05-11 17:44:55 +0200

    Fix shellcheck warning and make sure that downloadgenome workflow will not fail with spaces in paths

--- data/downloadgenome.sh
+++ data/downloadgenome.sh
@@ -8,6 +8,17 @@ notExists() {
 	[ ! -f "$1" ]
 }
 
+ARR=""
+push_back() {
+    # shellcheck disable=SC1003
+    CURR="$(printf '%s' "$1" | awk '{ gsub(/'\''/, "'\''\\'\'''\''"); print; }')"
+    if [ -z "$ARR" ]; then
+        ARR=''\'$CURR\'''
+    else
+        ARR=$ARR' '\'$CURR\'''
+    fi
+}
+
 [ -z "$MMSEQS" ] && echo "Please set the environment variable \$MMSEQS to your MMSEQS binary." && exit 1;
 
 hasCommand () {
@@ -23,30 +34,30 @@ OUTDB="$3"
 TMP_PATH="$4"
 
 #change path
-INPUTS=""
 if notExists "${OUT_PATH}/phage_download.complete"; then
     echo "Download phage genomes"
-    while read NAME URL; do
+    while read -r NAME URL; do
         wget -nv -O "${OUT_PATH}/${NAME}" "${URL}"
-        INPUTS="${OUT_PATH}/${NAME} ${INPUTS}"
+        push_back "${OUT_PATH}/${NAME}"
     done <  "${GENOME_FTP}"
     touch "${OUT_PATH}/phage_download.complete" \
         || fail "download failed"
     
 else
-    while read NAME URL; do
-        INPUTS="${OUT_PATH}/${NAME} ${INPUTS}";
+    while read -r NAME URL; do
+        push_back "${OUT_PATH}/${NAME}"
     done < "${GENOME_FTP}"
 fi
 
+eval "set -- $ARR"
 if notExists "${OUTDB}.index"; then
     # shellcheck disable=SC2086
-    "${MMSEQS}" createsetdb ${INPUTS} "${OUTDB}" "${TMP_PATH}" ${VERBOSITY_PAR}  \
+    "${MMSEQS}" createsetdb "${@}" "${OUTDB}" "${TMP_PATH}" ${VERBOSITY_PAR} \
         || fail "createsetdb failed"
 fi
 
 if [ -n "$CREATE_REVERSE_SETDB" ] && notExists "${OUTDB}_rev.index"; then
     # shellcheck disable=SC2086
-    "${MMSEQS}" createsetdb ${INPUTS} "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 \
+    "${MMSEQS}" createsetdb "${@}" "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 ${VERBOSITY_PAR} \
         || fail "create reverse setdb failed"
 fi
