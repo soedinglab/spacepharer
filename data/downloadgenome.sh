8fbcfec16550faf48d31029de33c0bf8e7a663ea
Milot Mirdita <milot@mirdita.de>
2020-11-21 02:41:10 +0100

    Fix whitespace in downloadgenome

--- data/downloadgenome.sh
+++ data/downloadgenome.sh
@@ -5,7 +5,7 @@ fail() {
 }
 
 notExists() {
-	[ ! -f "$1" ]
+	  [ ! -f "$1" ]
 }
 
 ARR=""
@@ -43,78 +43,80 @@ hasCommand wget
 
 OUTDB="$(abspath "$2")"
 TMP_PATH="$(abspath "$3")"
-MMSEQS="$(abspath "${MMSEQS}")"
 if [ -n "${GENOME_FTP}" ]; then
-  GENOME_FTP="$(abspath "${GENOME_FTP}")"
+    GENOME_FTP="$(abspath "${GENOME_FTP}")"
 fi
 
 cd "${TMP_PATH}"
 if [ -n "${GENOME_FTP}" ]; then
-  if notExists downloaded.tsv; then
-      : > downloaded.tsv.tmp
-      while read -r URL; do
-          NAME="$(basename "${URL}")"
-          if wget -N -np -nv "${URL}"; then
-              push_back "${NAME}"
-              echo "${NAME}" >> downloaded.tsv.tmp
-          fi
-      done < "${GENOME_FTP}"
-      mv -f downloaded.tsv.tmp downloaded.tsv
-  else
-      while read -r NAME; do
-          push_back "${NAME}"
-      done < downloaded.tsv
-  fi
-  eval "set -- $ARR"
-  if notExists "${OUTDB}.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" createsetdb "${@}" "${OUTDB}" "${TMP_PATH}" --reverse-fragments 0 ${CREATESETDB_PAR} \
-          || fail "createsetdb failed"
-  fi
+    if notExists downloaded.tsv; then
+        : > downloaded.tsv.tmp
+        while read -r URL; do
+            NAME="$(basename "${URL}")"
+            if wget -N -np -nv "${URL}"; then
+                push_back "${NAME}"
+                echo "${NAME}" >> downloaded.tsv.tmp
+            fi
+        done < "${GENOME_FTP}"
+        mv -f downloaded.tsv.tmp downloaded.tsv
+    else
+        while read -r NAME; do
+            push_back "${NAME}"
+        done < downloaded.tsv
+    fi
+    eval "set -- $ARR"
+    if notExists "${OUTDB}.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" createsetdb "${@}" "${OUTDB}" "${TMP_PATH}" --reverse-fragments 0 ${CREATESETDB_PAR} \
+            || fail "createsetdb failed"
+    fi
 
-  if [ -n "${CREATE_REVERSE_SETDB}" ] && notExists "${OUTDB}_rev.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" createsetdb "${@}" "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 ${CREATESETDB_PAR} \
-          || fail "create reverse setdb failed"
-  fi
+    if [ -n "${CREATE_REVERSE_SETDB}" ] && notExists "${OUTDB}_rev.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" createsetdb "${@}" "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 ${CREATESETDB_PAR} \
+            || fail "create reverse setdb failed"
+    fi
 else
-  case "${DBNAME}" in
-    "GenBank_phage_2018_09")
-        wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_phages_2018_09.tar";
-        IN_TAR="genbank_phages_2018_09.tar"
-    ;;
-    "GenBank_eukvir_2018_09")
-        wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_eukvir_2018_09.tar";
-        IN_TAR="genbank_eukvir_2018_09.tar"
-    ;;
-    "spacers_shmakov_et_al_2017")
-        wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/spacers_shmakov_et_al_2017.tar.gz";
-        IN_TAR="spacers_shmakov_et_al_2017.tar.gz"
-    ;;
-  esac
+    case "${DBNAME}" in
+      "GenBank_phage_2018_09")
+          wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_phages_2018_09.tar";
+          wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_phages_2018_09.tsv";
+          IN_TAR="genbank_phages_2018_09.tar"
+          IN_TAX="genbank_phages_2018_09.tsv"
+      ;;
+      "GenBank_eukvir_2018_09")
+          wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_eukvir_2018_09.tar";
+          wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/genbank_eukvir_2018_09.tsv";
+          IN_TAR="genbank_eukvir_2018_09.tar"
+          IN_TAX="genbank_eukvir_2018_09.tsv"
+      ;;
+      "spacers_shmakov_et_al_2017")
+          wget -N -np -nv "http://wwwuser.gwdg.de/~compbiol/spacepharer/2018_09/spacers_shmakov_et_al_2017.tar.gz";
+          IN_TAR="spacers_shmakov_et_al_2017.tar.gz"
+      ;;
+    esac
 
-  if notExists "${TMP_PATH}/tardb.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" tar2db "${IN_TAR}" "${TMP_PATH}/tardb" ${THREADS_PAR} \
-          || fail "tar2db failed"
-  fi
+    if notExists "${TMP_PATH}/tardb.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" tar2db "${IN_TAR}" "${TMP_PATH}/tardb" ${THREADS_PAR} \
+            || fail "tar2db failed"
+    fi
 
-  if notExists "${TMP_PATH}/seqdb.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" createdb "${TMP_PATH}/tardb" "${TMP_PATH}/seqdb" ${VERBOSITY_PAR} \
-          || fail "createdb failed"
-  fi
+    if notExists "${TMP_PATH}/seqdb.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" createdb "${TMP_PATH}/tardb" "${TMP_PATH}/seqdb" ${VERBOSITY_PAR} \
+            || fail "createdb failed"
+    fi
 
-  if notExists "${OUTDB}.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" createsetdb "${TMP_PATH}/seqdb" "${OUTDB}" "${TMP_PATH}" --reverse-fragments 0 ${CREATESETDB_PAR} \
-          || fail "createsetdb failed"
-  fi
+    if notExists "${OUTDB}.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" createsetdb "${TMP_PATH}/seqdb" "${OUTDB}" "${TMP_PATH}" --reverse-fragments 0 ${CREATESETDB_PAR} \
+            || fail "createsetdb failed"
+    fi
 
-  if [ -n "${CREATE_REVERSE_SETDB}" ] && notExists "${OUTDB}_rev.index"; then
-      # shellcheck disable=SC2086
-      "${MMSEQS}" createsetdb "${TMP_PATH}/seqdb" "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 ${CREATESETDB_PAR} \
-          || fail "create reverse setdb failed"
-  fi
+    if [ -n "${CREATE_REVERSE_SETDB}" ] && notExists "${OUTDB}_rev.index"; then
+        # shellcheck disable=SC2086
+        "${MMSEQS}" createsetdb "${TMP_PATH}/seqdb" "${OUTDB}_rev" "${TMP_PATH}" --reverse-fragments 1 ${CREATESETDB_PAR} \
+            || fail "create reverse setdb failed"
+    fi
 fi
-
